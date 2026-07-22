c5b321c8e3f56264a1ee828f3bf7d8b533dbf21b
Daniel Meyer <dansebmey@gmail.com>
2026-07-22 12:33:50 +0200

    fix Light/Heavy Metal not affecting the right mon

--- src/battle_util.c
+++ src/battle_util.c
@@ -4648,7 +4648,7 @@ u32 AbilityBattleEffects(u32 caseID, u32 battler, u32 ability, u32 special, u32 
             if (!gSpecialStatuses[battler].switchInAbilityDone)
             {
                 gSpecialStatuses[battler].switchInAbilityDone = TRUE;
-                // PREPARE_TYPE_BUFFER(gBattleTextBuff1, gBattleMons[battler].type1);
+                gBattlerTarget = battler;
                 BattleScriptPushCursorAndCallback(BattleScript_D2D_LightMetalActivates);
                 effect++;
             }
@@ -4657,7 +4657,7 @@ u32 AbilityBattleEffects(u32 caseID, u32 battler, u32 ability, u32 special, u32 
             if (!gSpecialStatuses[battler].switchInAbilityDone)
             {
                 gSpecialStatuses[battler].switchInAbilityDone = TRUE;
-                // PREPARE_TYPE_BUFFER(gBattleTextBuff1, gBattleMons[battler].type1);
+                gBattlerTarget = battler;
                 BattleScriptPushCursorAndCallback(BattleScript_D2D_HeavyMetalActivates);
                 effect++;
             }
